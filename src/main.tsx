e6e933e34e4b1c85111bcd80fa5c17fb443144fe
Harmeet Singh <harmeet.jaipur@gmail.com>
2024-05-23 12:18:20 -0400

    add dev mode error logging for apollo

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ApolloProvider } from '@apollo/client';
+import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev";
 
 import client from './apolloClient';
 import './index.css';
+
+if (process.env.NODE_ENV === 'development') {
+  loadDevMessages();
+  loadErrorMessages();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
