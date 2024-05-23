64ce2af7d94d623ab4561489dc5db3da3041f2aa
Harmeet Singh <harmeet.jaipur@gmail.com>
2024-05-22 21:08:48 -0400

    add queries and mutation

---
+++ src/graphql/GET_POST.ts
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+
+export const GET_POST = gql`
+  query GetPost($id: ID!) {
+    post(id: $id) {
+      id
+      title
+      description
+      createdAt
+      fields {
+        key
+        value
+      }
+      thumbnail {
+        ... on Image {
+          url
+        }
+      }
+      owner {
+        member {
+          displayName
+        }
+      }
+    }
+  }
+`;
