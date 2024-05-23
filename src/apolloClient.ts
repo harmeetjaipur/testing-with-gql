fc75f419f6d6a22b56b7077540be5688925d4a8c
Harmeet Singh <harmeet.jaipur@gmail.com>
2024-05-22 21:23:45 -0400

    update whitespace styling

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -2,24 +2,24 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'https://api.bettermode.com',
+    uri: 'https://api.bettermode.com',
 });
 
 const authLink = setContext((_, { headers }) => {
-  // Get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
-  // Return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
+    // Get the authentication token from local storage if it exists
+    const token = localStorage.getItem('token');
+    // Return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
 });
 
 export default client;
